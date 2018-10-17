cd943db74c26bf1a2af4ebd5659b97676e745fb8
Michail Samartzis <michailangelo@gmail.com>
2018-10-17 16:30:54 +0300

    testing the new branch.

--- src/GitPluginTestApp.java
+++ src/GitPluginTestApp.java
@@ -3,7 +3,7 @@ public class GitPluginTestApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("");
+		System.out.println("Hello Git, from branch bug#0001!");
 
 	}
 
